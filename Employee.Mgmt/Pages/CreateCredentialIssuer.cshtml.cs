7f03b9d1af46b8e02c91d47ec04326e21fead222
damienbod <damien_bod@hotmail.com>
2025-06-26 07:59:53 +0200

    Init QrCode generation

--- Employee.Mgmt/Pages/CreateCredentialIssuer.cshtml.cs
+++ Employee.Mgmt/Pages/CreateCredentialIssuer.cshtml.cs
@@ -1,6 +1,7 @@
 using Employee.Mgmt.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Net.Codecrete.QrCodeGenerator;
 using System.Text.Json;
 
 namespace Employee.Mgmt.Pages;
@@ -12,6 +13,9 @@ public class CreateCredentialIssuerModel : PageModel
     [BindProperty]
     public string? QrCodeUrl { get; set; } = null;
 
+    [BindProperty]
+    public byte[]? QrCodePng { get; set; } = null;
+
     public CreateCredentialIssuerModel(CreateIssuer createIssuer)
     {
         _createIssuer = createIssuer;
@@ -21,11 +25,18 @@ public class CreateCredentialIssuerModel : PageModel
     {
     }
 
+    /// <summary>
+    /// QrCode.Ecc.Low, QrCode.Ecc.Medium, QrCode.Ecc.Quartile, QrCode.Ecc.High
+    /// </summary>
+    /// <returns></returns>
     public async Task OnPostAsync()
     {
         var vci = await _createIssuer.IssuerCredentialAsync();
 
         var data = JsonSerializer.Deserialize<CredentialIssuerModel>(vci);
+
+        var qrCode = QrCode.EncodeText(data!.offer_deeplink, QrCode.Ecc.Quartile);
+        QrCodePng = qrCode.ToPng(20, 4);
 
         QrCodeUrl = data!.offer_deeplink;
     }
