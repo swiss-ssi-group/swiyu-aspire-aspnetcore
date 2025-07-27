33ee536255ea360a5ee5611a8fc3fd8ab52105e5
damienbod <damien_bod@hotmail.com>
2025-07-27 11:43:31 +0200

    clean up

--- Swiyu.Aspire.Mgmt/Program.cs
+++ Swiyu.Aspire.Mgmt/Program.cs
@@ -1,44 +1,43 @@
 using Swiyu.Aspire.Mgmt.Services;
 
-namespace Swiyu.Aspire.Mgmt
+namespace Swiyu.Aspire.Mgmt;
+
+public static class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddScoped<VerificationService>();
+        builder.Services.AddScoped<IssuerService>();
+
+        builder.Services.AddHttpClient();
+        builder.Services.AddOptions();
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddScoped<VerificationService>();
-            builder.Services.AddScoped<IssuerService>();
-
-            builder.Services.AddHttpClient();
-            builder.Services.AddOptions();
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-            builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapStaticAssets();
-            app.MapRazorPages()
-               .WithStaticAssets();
-            app.MapControllers();
-
-            app.Run();
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+        app.MapRazorPages()
+           .WithStaticAssets();
+        app.MapControllers();
+
+        app.Run();
     }
 }
