99f75c78d94b7da479f60bedbf977f381732dd3c
damienbod <damien_bod@hotmail.com>
2025-07-27 21:07:43 +0200

    clean up

--- Swiyu.Aspire.Mgmt/Services/CredentialIssuerModel.cs
+++ Swiyu.Aspire.Mgmt/Services/CredentialIssuerModel.cs
@@ -5,7 +5,7 @@ namespace Swiyu.Aspire.Mgmt.Services;
 public class CredentialIssuerModel
 {
     [JsonPropertyName("management_id")]
-    public string management_id { get; set; }
+    public string management_id { get; set; } = null!;
     [JsonPropertyName("offer_deeplink")]
-    public string offer_deeplink { get; set; }
+    public string offer_deeplink { get; set; } = null!;
 }
