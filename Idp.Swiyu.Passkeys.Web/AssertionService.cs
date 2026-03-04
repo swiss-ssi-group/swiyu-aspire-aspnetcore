4c47c8a1f8b5996f681ded53cc9a1a81359698e0
damienbod <damien_bod@hotmail.com>
2026-03-04 15:14:32 +0100

    Update and use configuration

--- Idp.Swiyu.Passkeys.Web/AssertionService.cs
+++ Idp.Swiyu.Passkeys.Web/AssertionService.cs
@@ -18,8 +18,11 @@ public static class AssertionService
         var clientId = configuration.GetValue<string>("WebOidcClientId");
         var authority = configuration.GetValue<string>("WebOidcAuthority");
 
-        var privatePem = File.ReadAllText(Path.Combine("", "rsa256-oidc-private.pem"));
-        var publicPem = File.ReadAllText(Path.Combine("", "rsa256-oidc-public.pem"));
+        var privatePem = configuration.GetValue<string>("WebOidcClientPrivatePem");
+        var publicPem = configuration.GetValue<string>("WebOidcClientPublicPem");
+        //var privatePem = File.ReadAllText(Path.Combine("", "rsa256-oidc-private.pem"));
+        //var publicPem = File.ReadAllText(Path.Combine("", "rsa256-oidc-public.pem"));
+
         var rsaCertificate = X509Certificate2.CreateFromPem(publicPem, privatePem);
         var rsaCertificateKey = new RsaSecurityKey(rsaCertificate.GetRSAPrivateKey());
         var signingCredentials = new SigningCredentials(new X509SecurityKey(rsaCertificate), "RS256");
@@ -52,8 +55,8 @@ public static class AssertionService
         var clientId = configuration.GetValue<string>("WebOidcClientId");
         var authority = configuration.GetValue<string>("WebOidcAuthority");
 
-        //var privatePem = File.ReadAllText(Path.Combine("", "rsa256-private.pem"));
-        //var publicPem = File.ReadAllText(Path.Combine("", "rsa256-public.pem"));
+        //var privatePem = File.ReadAllText(Path.Combine("", "rsa256-oidc-private.pem"));
+        //var publicPem = File.ReadAllText(Path.Combine("", "rsa256-oidc-public.pem"));
 
         var privatePem = configuration.GetValue<string>("WebOidcClientPrivatePem");
         var publicPem = configuration.GetValue<string>("WebOidcClientPublicPem");
