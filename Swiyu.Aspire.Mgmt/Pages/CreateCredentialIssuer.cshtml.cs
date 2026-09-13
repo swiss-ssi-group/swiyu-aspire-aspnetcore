efd57291e51eb3c3df08de3d754ff56359602d6c
damienbod <damien_bod@hotmail.com>
2026-09-13 09:34:53 +0200

    Update

--- Swiyu.Aspire.Mgmt/Pages/CreateCredentialIssuer.cshtml.cs
+++ Swiyu.Aspire.Mgmt/Pages/CreateCredentialIssuer.cshtml.cs
@@ -18,6 +18,9 @@ public class CreateCredentialIssuerModel : PageModel
     public byte[] QrCodePng { get; set; } = [];
 
     [BindProperty]
+    public string? DeepLink { get; set; } = string.Empty;
+
+    [BindProperty]
     public string? ManagementId { get; set; } = null;
 
     public CreateCredentialIssuerModel(IssuerService issuerService)
@@ -50,6 +53,7 @@ public class CreateCredentialIssuerModel : PageModel
         QrCodePng = qrCode.ToPng(20, 4, MagickColors.Black, MagickColors.White);
 
         QrCodeUrl = data!.offer_deeplink;
+        DeepLink = data.offer_deeplink;
         ManagementId = data!.management_id;
     }
 }
