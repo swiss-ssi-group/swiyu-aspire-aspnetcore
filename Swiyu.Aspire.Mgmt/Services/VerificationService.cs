e6a1cb94999c92e28b2afb6a9bcaa462f6a11806
damienbod <damien_bod@hotmail.com>
2026-09-15 10:49:49 +0200

    Cleanup

--- Swiyu.Aspire.Mgmt/Services/VerificationService.cs
+++ Swiyu.Aspire.Mgmt/Services/VerificationService.cs
@@ -37,7 +37,7 @@ public class VerificationService
         var acceptedIssuerDid = "did:tdw:QmPEZPhDFR4nEYSFK5bMnvECqdpf1tPTPJuWs9QrMjCumw:identifier-reg.trust-infra.swiyu-int.admin.ch:api:v1:did:9a5559f0-b81c-4368-a170-e7b4ae424527";
 
         var inputDescriptorsId = Guid.NewGuid().ToString();
-        var presentationDefinitionId = "urn:vct:ch.admin.bcs.betaid"; // Guid.NewGuid().ToString();
+        var presentationDefinitionId = "00000000-0000-0000-0000-000000000000"; // Guid.NewGuid().ToString();
 
         var json = GetBetaIdVerificationPresentationBodyV4(inputDescriptorsId,
             presentationDefinitionId, acceptedIssuerDid, "urn:vct:ch.admin.bcs.betaid");
@@ -56,7 +56,7 @@ public class VerificationService
         _logger.LogInformation("Creating verification presentation");
 
         var inputDescriptorsId = Guid.NewGuid().ToString();
-        var presentationDefinitionId = IssuerService.DAMIENBOD_VC; // Guid.NewGuid().ToString();
+        var presentationDefinitionId = "00000000-0000-0000-0000-000000000000"; // Guid.NewGuid().ToString();
 
         var json = GetDataForLocalCredential(inputDescriptorsId, presentationDefinitionId, _issuerId!, IssuerService.DAMIENBOD_VC);
 
